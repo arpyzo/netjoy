e5ddc4591b799f9d98273b571d9f853a5b65e952
Robert Pyzalski <pyzals@gmail.com>
2014-09-26 20:14:12 -0400

    adding poc code to extract packet details

--- main.cpp
+++ main.cpp
@@ -89,26 +89,44 @@ bool App::Init_NIC() {
 		}
 	}
 
-    return TRUE;
+    //return TRUE;
 
-	/*if ((fp = pcap_open(nics->next->name, 100, PCAP_OPENFLAG_PROMISCUOUS, 20, NULL, error_buffer)) == NULL) {
+	if ((fp = pcap_open(nics->next->name, 100, PCAP_OPENFLAG_PROMISCUOUS, 20, NULL, error_buffer)) == NULL) {
 		wxMessageBox(wxString::Format("Error opening source: %s", error_buffer));
 	} else {
 		// Read the packets
-		while ((res = pcap_next_ex(fp, &header, &pkt_data)) >= 0) {
+        int num_packets = 0;
+		while ((res = pcap_next_ex(fp, &header, &pkt_data)) >= 0 && num_packets < 10) {
 
 			if (res == 0)
 				// Timeout elapsed
 				continue;
-
+            num_packets++;
 			// print pkt timestamp and pkt len
 			wxMessageBox(wxString::Format("%ld:%ld (%ld)\n", header->ts.tv_sec, header->ts.tv_usec, header->len));
 
+                ip_header *ih = (ip_header *) (pkt_data + 14);
+
 			// Print the packet
-			for (int i = 1; (i < header->caplen + 1); i++) {
+			/*for (int i = 1; (i < header->caplen + 1); i++) {
 				printf("%.2x ", pkt_data[i - 1]);
 				if ((i % 16) == 0) printf("\n");
-			}
+			}*/
+
+                  /* print ip addresses and udp ports */
+    //printf("%d.%d.%d.%d.%d -> %d.%d.%d.%d.%d\n",
+    wxMessageBox(wxString::Format("%d.%d.%d.%d -> %d.%d.%d.%d\n",
+        ih->saddr.byte1,
+        ih->saddr.byte2,
+        ih->saddr.byte3,
+        ih->saddr.byte4,
+        //sport,
+        ih->daddr.byte1,
+        ih->daddr.byte2,
+        ih->daddr.byte3,
+        ih->daddr.byte4
+        //dport
+        ));
 
 			printf("\n\n");
 		}
@@ -116,5 +134,5 @@ bool App::Init_NIC() {
 		if (res == -1) {
 			wxMessageBox(wxString::Format("Error reading the packets: %s\n", pcap_geterr(fp)));
 		}
-	}*/
+	}
 }
